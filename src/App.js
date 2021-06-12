93bb362863d4f5b0455e805dac35f4bdf545911a
Jones Zachariah Noel <joneszachariahnoel@gmail.com>
2021-06-12 15:22:48 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import {AmplifyS3Album} from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifyAuthenticator>
+        <header className="App-header">
+          <AmplifySignOut />
+          <AmplifyS3Album />
+        </header>
+      </AmplifyAuthenticator>
     </div>
   );
 }
